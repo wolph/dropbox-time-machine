53d4aba7a60d5bf99e5242d3b62905a91aa48648
WoLpH <Rick@Fawo.nl>
2013-03-09 22:51:46 +0100

    added serializable dropbox sessions for celery

--- time_machine/dropbox_flask_session.py
+++ time_machine/dropbox_flask_session.py
@@ -4,15 +4,38 @@ import dropbox
 
 class DropboxSession(dropbox.session.DropboxSession):
 
+    def __init__(self, session=None):
+        from main import app
+        dropbox.session.DropboxSession.__init__(
+            self,
+            consumer_key=app.config['APP_KEY'],
+            consumer_secret=app.config['APP_SECRET'],
+            access_type=app.config['ACCESS_TYPE'],
+        )
+
+        session = self.session = session or flask.session
+        if session.get('request_token') and session.get(
+                'request_token_secret'):
+            # Set the request token if available
+            self.set_request_token(
+                request_token=session['request_token'],
+                request_token_secret=session['request_token_secret'],
+            )
+
+        if session.get('access_token') and session.get('access_token_secret'):
+            # Set the access token if available
+            self.set_token(
+                access_token=session['access_token'],
+                access_token_secret=session['access_token_secret'],
+            )
+
     def link(self, force=False, url=None):
         # Only build a new request token if we don't have one or are forced to
         # do so.
         if force or not self.request_token:
-            print 'obtaining request token'
             self.obtain_request_token()
         else:
             try:
-                print 'obtaining access token'
                 self.obtain_access_token()
             except dropbox.rest.ErrorResponse, exception:
                 if exception.status == 401:
@@ -29,16 +52,14 @@ class DropboxSession(dropbox.session.DropboxSession):
         This updates the DropboxSession so we have to notify Flask that our
         session has changed.
         '''
-        print 'building request token'
         request_token = dropbox.session.DropboxSession.obtain_request_token(
             self)
 
-        flask.session['request_token'] = self.request_token.key
-        flask.session['request_token_secret'] = self.request_token.secret
+        self.session['request_token'] = self.request_token.key
+        self.session['request_token_secret'] = self.request_token.secret
         return request_token
 
     def obtain_access_token(self, request_token=None):
-        print 'building access token'
         '''
         This updates the DropboxSession so we have to notify Flask that our
         session has changed.
@@ -46,40 +67,15 @@ class DropboxSession(dropbox.session.DropboxSession):
         access_token = dropbox.session.DropboxSession.obtain_access_token(
             self, request_token)
 
-        flask.session['access_token'] = self.token.key
-        flask.session['access_token_secret'] = self.token.secret
+        self.session['access_token'] = self.token.key
+        self.session['access_token_secret'] = self.token.secret
         return access_token
 
     def unlink(self):
         self.request_token = None
-        flask.session['request_token'] = None
-        flask.session['request_token_secret'] = None
-        flask.session['access_token'] = None
-        flask.session['access_token_secret'] = None
+        self.session['request_token'] = None
+        self.session['request_token_secret'] = None
+        self.session['access_token'] = None
+        self.session['access_token_secret'] = None
         return dropbox.session.DropboxSession.unlink(self)
-
-    def __init__(self):
-        from main import app
-        dropbox.session.DropboxSession.__init__(
-            self,
-            consumer_key=app.config['APP_KEY'],
-            consumer_secret=app.config['APP_SECRET'],
-            access_type=app.config['ACCESS_TYPE'],
-        )
-
-        data = flask.session
-
-        if data.get('request_token') and data.get('request_token_secret'):
-            # Set the request token if available
-            self.set_request_token(
-                request_token=data['request_token'],
-                request_token_secret=data['request_token_secret'],
-            )
-
-        if data.get('access_token') and data.get('access_token_secret'):
-            # Set the access token if available
-            self.set_token(
-                access_token=data['access_token'],
-                access_token_secret=data['access_token_secret'],
-            )
 
