73249ca5b84e34e28a4ad672dfba3f123b9d6fed
WoLpH <Rick@Fawo.nl>
2013-03-10 01:02:52 +0100

    added exampel settings file

---
+++ time_machine/example_settings.py
@@ -0,0 +1,13 @@
+# Customize these variables to get everything to work ;)
+
+# Authentication data
+APP_KEY = ''
+APP_SECRET = ''
+ACCESS_TYPE = ''
+
+# Secret key for sessions
+SECRET_KEY = ''
+
+# Celery broker
+BROKER_URL = 'redis://'
+
